666787285042d68ae994527736304d94d164d535
Henry Smith <h.f.smithz99@gmail.com>
2023-10-03 12:29:56 -0400

    complete

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 /*----- constants -----*/
 const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-countdown-922.mp3');
 
+const RPS_LOOKUP = {
+    r: {img: 'imgs/rock.png', beats: 's' },
+    p: {img: 'imgs/paper.png', beats: 'r'},
+    s: {img: 'imgs/scissors.png', beats: 'p'}
+};
+
 /*----- app's state (variables) -----*/
 let scores; //Object key of 'p' -> player, key of 't'-> tie, key of 'c' -> computer
 
@@ -10,9 +16,12 @@ let results; //Object key of 'p' for player results, key of 'c' for computer res
 let winner; //String 'p' if player wins, 't' for a tie, 'c' if computer wins
 /*----- cached element references -----*/
 
+const pResultEL = document.getElementById('p-result');
+const cResultEL = document.getElementById('c-result');
+const countdownEL = document.getElementById('countdown'); 
 
 /*----- event listeners -----*/
-
+document.querySelector('main').addEventListener('click', handleChoice);
 
 /*----- functions -----*/
 init();
@@ -32,6 +41,29 @@ function init() {
     render();
 }
 
+function handleChoice(evt){
+    //gaurds
+    if(evt.target.tagName !== 'BUTTON') return;
+    //player has made a choice
+    results.p = evt.target.innerText.toLowerCase();
+    //compute random choice for computer
+    results.c = getRandomRPS();
+    winner = getWinner();
+    scores[winner] += 1;
+    render();
+}
+
+function getWinner (){
+    if (results.p === results.c) return 't';
+    return RPS_LOOKUP[results.p].beats === results.c ? 'p' : 'c';
+}
+
+function getRandomRPS() {
+    const rps = Object.keys(RPS_LOOKUP);
+    const rndIDX = Math.floor(Math.random() * rps.length);
+    return rps[rndIDX];
+}
+
 function renderScores() {
     for (let key in scores) {
         const scoreEl = document.getElementById(`${key}-score`);
@@ -40,11 +72,35 @@ function renderScores() {
 }
 
 function renderResults() {
+    pResultEL.src = RPS_LOOKUP[results.p].img;
+    cResultEL.src = RPS_LOOKUP[results.c].img;
+    pResultEL.style.borderColor = winner === 'p' ? 'grey' : 'white';
+    cResultEL.style.borderColor = winner === 'c' ? 'grey' : 'white';
 
 }
 
-
 function render() {
-    renderScores();
-    renderResults();
+    renderCountdown(function() {
+        renderScores();
+        renderResults();
+    });
+}
+
+function renderCountdown(cb) {
+    let count = 3;
+    AUDIO.currentTime = 0;
+    AUDIO.play();
+    countdownEL.style.visibility = 'visible';
+    countdownEL.innerText = count;
+    
+    const timerId = setInterval(function() {
+        count--;
+        if(count){
+            countdownEL.innerText = count;
+        } else {
+            clearInterval(timerId);
+            countdownEL.style.visibility = 'hidden';
+            cb();
+        }
+    }, 1000)
 }
